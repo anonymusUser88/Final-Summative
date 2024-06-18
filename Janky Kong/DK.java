1ed239870d08fd8c62af10166f95292d31cc051f
ilovedanny <dannyjudge1648@gmail.com>
2024-06-17 20:59:00 -0400

    Done lvl 2

--- Janky Kong/DK.java
+++ Janky Kong/DK.java
@@ -1,9 +1,18 @@
 import greenfoot.*;
+<<<<<<< Updated upstream
+=======
+
+/**
+ * DK Class
+ * June 17, 2024
+ */
+>>>>>>> Stashed changes
 public class DK extends Actor
 {
     long lastTimeSinceBarrelThrown; // throwing the barrel
     long lastTimeSinceDKMoves; // animation
     int randomNum = Greenfoot.getRandomNumber(15) + 1;
+<<<<<<< Updated upstream
     public DK() {
         scaleImage();
     }
@@ -13,6 +22,40 @@ public class DK extends Actor
         {
             Greenfoot.setWorld(new Finish());
 
+=======
+    int Yshift = 50; // used in gameover screen to shift dk
+    Boolean playersWon = false;
+    long TimeofDeath;
+    static int level = 1;
+
+    public DK()
+    {
+        setImageScaled("Donkey_Kong_Standing.png");
+    }
+
+    public void act() 
+    {
+        System.out.println(level);
+
+        Mario m = getWorld().getObjects(Mario.class).get(0); // gets the mario object
+        Luigi l = getWorld().getObjects(Luigi.class).get(0); // gets the luigi object
+
+        if((isTouching(Mario.class) || isTouching(Luigi.class)) && level == 1) // if either of the players reach donkey kong
+        {
+            Level2 level2 = new Level2();
+            Greenfoot.setWorld(level2);
+            level++;
+        }  else if ((isTouching(Mario.class) || isTouching(Luigi.class)) && level == 2) {
+            TimeofDeath = System.currentTimeMillis();
+            playersWon = true;
+            getWorld().showText("YOU WIN", 750, 600);
+            GreenfootSound music = new GreenfootSound("Donkey Kong Arcade Music - Round Clear.mp3");
+            music.play();
+        }
+
+        if(getY()>770)
+        {
+>>>>>>> Stashed changes
             Greenfoot.stop();
         }
 
--- Janky Kong/Princess.java
+++ Janky Kong/Princess.java
@@ -14,6 +14,52 @@ public class Princess extends Actor
      */
     public void act() 
     {
+<<<<<<< Updated upstream
         // Add your action code here.
     }    
+=======
+        DK d = getWorld().getObjects(DK.class).get(0); // gets the DK object
+
+        if(d.playersWon == false)
+        {
+            if(System.currentTimeMillis() - TimeSinceLastAnimation > 1000)
+            {
+                setImageScaled("Princess_Peach_.png",17);
+                if(DK.level == 1) {
+                    setLocation(60, 168);
+                } else if(DK.level == 2) {
+                    setLocation(60, 168 - 75);
+                }
+                if(System.currentTimeMillis() - TimeSinceHelpAnimation > 1500){
+                    TimeSinceHelpAnimation = System.currentTimeMillis();
+                    TimeSinceLastAnimation = System.currentTimeMillis();
+                    if(DK.level == 1) {
+                        setLocation(98, 152);
+                    } else if(DK.level == 2) {
+                        setLocation(98, 152 - 75);
+                    }
+                    setImage("princess_peach_help.png");
+
+                }
+
+            } else{
+                setImageScaled("Princess_Peach_.png",17);
+                if(DK.level == 1) {
+                    setLocation(60, 168);
+                } else if(DK.level == 2) {
+                    setLocation(60, 168 - 75);
+                }
+
+            }
+
+        }
+    }
+
+    private void setImageScaled(String image,int ScaleValue) {
+        GreenfootImage img = new GreenfootImage(image);
+        img.scale(img.getWidth() / ScaleValue, img.getHeight() / ScaleValue);
+        setImage(img);
+
+    }
+>>>>>>> Stashed changes
 }
